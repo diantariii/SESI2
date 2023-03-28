81449100768a77851e8d247f7c614427ab2f2088
kadekdiantarii_2201010330 <119943301+diantariii@users.noreply.github.com>
2023-03-28 14:54:18 +0800

    Add files via upload

---
+++ SESI2/latihan02.php
@@ -0,0 +1,13 @@
+<?php
+    $angka = 3;
+
+    echo "Nilai Angka : <strong>".$angka."</strong>";
+    #tanda . (titik) berfungsi untuk memisahkan elemen lain
+
+    if($angka>4){
+        //kondisi true
+        echo "<br>Nilai Angka Lebih dari 4";
+    }else{
+        //kondisi false
+        echo "<br>Nilai Angka Kurang dari 5"; 
+    }
